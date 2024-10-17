2a4f61b16efa4b4c0fe97d6d1f95b780ed486f1b
RAJJIT <2.0lairaj@gmail.com>
2024-10-17 22:34:18 +0530

    v1.01-changes.1

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-    printf("Hello, world!\n");
-    return 0;
-} 
+#include <X11/Xlib.h>
+#include <X11/X.h>
+
+Window win;
+
+int main(void)
+{
+    Display *dsp = XOpenDisplay(NULL);
+
+    win = XCreateSimpleWindow(dsp, DefaultRootWindow(dsp), 0, 0, 1280, 720, 0, 0, 0)
+
+        XMapWindow(dsp, win);
+    XFlush(dsp);
+
+    XEvent ev;
+
+    while (1)
+    {
+        XNextEvent(dsp, &ev);
+    }
+
+    XDestroyWindow(dsp, win);
+    XCloseDisplay(dsp);
+}
