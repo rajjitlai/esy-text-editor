dbde77b9379a4a104cfeb56c5263492e40691ebd
RAJJIT <2.0lairaj@gmail.com>
2026-06-02 11:48:46 +0530

    reset: wanted to make it via electron

--- Makefile
+++
@@ -1,6 +0,0 @@
-CC = gcc
-CFLAGS = -Wall $(shell pkg-config --cflags x11 xft fontconfig)
-LIBS = $(shell pkg-config --libs x11 xft fontconfig)
-
-all:
-	$(CC) $(CFLAGS) -o editor *.c $(LIBS)
--- file_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
-#include "buffer.h"
-
-int buffer_load(Buffer *buf, const char *filename);
-int buffer_save(Buffer *buf, const char *filename);
-
-#endif
